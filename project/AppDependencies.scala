0e41865d7e12b53f29e7a5c8e47ea444ad880589
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2026-01-13 13:32:43 +0000

    API-8890 - Version bump

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "10.5.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
@@ -12,6 +12,6 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.29"
+    "org.mockito" %% "mockito-scala-scalatest" % "2.0.0"
   ).map(_ % "test")
 }
